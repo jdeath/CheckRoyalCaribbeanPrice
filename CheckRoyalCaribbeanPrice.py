c7c67a42b0646dfb84e5405901ce0761382bc73f
jdeath <17914369+jdeath@users.noreply.github.com>
2025-03-30 22:23:21 -0400

    Add support to price cruise itself

--- CheckRoyalCaribbeanPrice.py
+++ CheckRoyalCaribbeanPrice.py
@@ -2,8 +2,12 @@ import requests
 import yaml
 from apprise import Apprise
 from datetime import datetime
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs
+import re
 
 def main():
+    
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(timestamp)
     
@@ -11,18 +15,30 @@ def main():
         
     with open('config.yaml', 'r') as file:
         data = yaml.safe_load(file)
-        username = data['username']
-        password = data['password']
-        accountId = data['accountId']
+        
+        
         if 'apprise' in data:
             for apprise in data['apprise']:
                 url = apprise['url']
                 apobj.add(url)
-                
-    session = requests.session()
-    access_token,session = login(username,password,session)
-    getVoyages(access_token,accountId,session,apobj)
     
+        if 'accountInfo' in data:
+            for accountInfo in data['accountInfo']:
+                username = accountInfo['username']
+                password = accountInfo['password']
+                accountId = accountInfo['accountId']
+    
+                print(username)
+                session = requests.session()
+                access_token,session = login(username,password,session)
+                getVoyages(access_token,accountId,session,apobj)
+    
+        if 'cruises' in data:
+            for cruises in data['cruises']:
+                cruiseURL = cruises['cruiseURL'] 
+                paidPrice = float(cruises['paidPrice'])
+                get_cruise_price(cruiseURL, paidPrice, apobj)
+                
             
 def login(username,password,session):
     headers = {
@@ -43,10 +59,11 @@ def login(username,password,session):
         'Priority': 'u=0',
     }
     
-
+    
 
     data = 'grant_type=password&username=' + username +  '&password=' + password + '&scope=openid+profile+email+vdsid'
-
+    
+    #data = 'grant_type=password&username=' + username +  '&password=' + password + '&scope=openid+profile+email'
     response = session.post('https://www.royalcaribbean.com/auth/oauth2/access_token', headers=headers, data=data)
     access_token = response.json().get("access_token")
     return access_token,session
@@ -93,12 +110,13 @@ def getNewBeveragePrice(access_token,accountId,session,reservationId,ship,startD
         params=params,
         headers=headers,
     )
-
+    
+    #print(response.json().get("payload"))
     title = response.json().get("payload").get("title")
     currentPrice = response.json().get("payload").get("startingFromPrice").get("adultPromotionalPrice")
     if not currentPrice:
         currentPrice = response.json().get("payload").get("startingFromPrice").get("adultShipboardPrice")
-        
+    
     if currentPrice < paidPrice:
         text = reservationId + ": " + title + " Price is lower: " + str(currentPrice) + " Rebook"
         print(text)
@@ -107,7 +125,7 @@ def getNewBeveragePrice(access_token,accountId,session,reservationId,ship,startD
         print(reservationId + ": " + "You have the best price for " + title +  " of: " + str(paidPrice))
         
     if currentPrice > paidPrice:
-        print(reservationId + ": \t" + "Price of " + title + " is now higher: " + str(currentPrice))
+        print(reservationId + ": \t " + "Price of " + title + " is now higher: " + str(currentPrice))
 
 def getVoyages(access_token,accountId,session,apobj):
 
@@ -149,7 +167,7 @@ def getVoyages(access_token,accountId,session,apobj):
         params=params,
         headers=headers,
     )
-    
+   
     
     for booking in response.json().get("payload").get("profileBookings"):
         reservationId = booking.get("bookingId")
@@ -230,7 +248,49 @@ def getOrders(access_token,accountId,session,reservationId,passengerId,ship,star
                paidPrice = round(paidPrice / numberOfNights,2)
                
             getNewBeveragePrice(access_token,accountId,session,reservationId,ship,startDate,prefix,paidPrice,product,apobj)
+
+def get_cruise_price(url, paidPrice, apobj):
+
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en-US,en;q=0.9',
+        'priority': 'u=0, i',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+    }
+
+    parsed_url = urlparse(url)
+    params = parse_qs(parsed_url.query)
     
+    response = requests.get('https://www.royalcaribbean.com/checkout/guest-info', params=params,headers=headers)
+
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    priceString = soup.find("span",attrs={"class":"SummaryPrice_title__1nizh9x5","data-testid":"pricing-total"}).text
+    
+    priceString = priceString.replace(",", "")
+    m = re.search("\$(.*)USD", priceString)
+    priceOnlyString = m.group(1)
+    price = float(priceOnlyString)
+    
+    preString = params.get("sailDate")[0] + " " + params.get("shipCode")[0]+ " " + params.get("cabinClassType")[0]
+    
+    if price < paidPrice: 
+        textString = "Rebook! " + preString + " New Price for "  + str(price) + " is lower than " + str(paidPrice)
+        print(textString)
+        apobj.notify(body=textString, title='Cruise Price Alert')
+    else:
+        print(preString + ": You have best Price of " + str(paidPrice) )
+        if price > paidPrice:
+            print("\t Current Price is higher: " + str(price) )
+
 if __name__ == "__main__":
     main()
  
