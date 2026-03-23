e1c33e00d35cb715e2c1b9d0513f7e071053b9e1
jdeath <17914369+jdeath@users.noreply.github.com>
2026-03-22 21:50:59 -0400

    Update BrowseRoyalCaribbeanPrice.py

--- BrowseRoyalCaribbeanPrice.py
+++ BrowseRoyalCaribbeanPrice.py
@@ -103,7 +103,6 @@ def main():
                 print("Direct Link To Celebrity Website: ")
                 linkRoot = "https://www.celebritycruises.com/account/cruise-planner/category/drinks"
                 
-            #This link is no longer working for a bogus bookingID
             print(f"{linkRoot}?bookingId=123456&shipCode={shipcode}&sailDate={sailing['date']}")
             print("")
             print("These are public prices, sale prices for you could be less")
